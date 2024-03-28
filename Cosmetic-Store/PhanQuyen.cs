6d989ad1da7e7dce7417a954d1a061f0806ea5a2
giaphat-nham <phatnham3010@gmail.com>
2024-03-28 21:42:54 +0700

    Giao dien quan ly tai khoan, phan quyen, chuc nang

---
+++ Cosmetic-Store/PhanQuyen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cosmetic_Store
+{
+    public partial class PhanQuyen : UserControl
+    {
+        public PhanQuyen()
+        {
+            InitializeComponent();
+        }
+    }
+}
