d03e8391b8d6033106e04d705acd357f74dac59c
Thien Huong <thienhuong6935@gmail.com>
2024-05-11 12:02:12 +0700

    update dg, code chuc nang  phan QLNS
    chuc nang QL thong tin ca nhan, thong tin nhan su,, chuc vu,  nghi phep, thong ke nhan vien theo chuc vu

--- ValueObject/Position.cs
+++ ValueObject/Position.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ValueObject
 {
-    class Position
+    public class Position
     {
+        private int positionID;
+        private string positionName;
+        private int salary;
+        private byte isDeleted;
+
+        public Position()
+        {
+                
+        }
+        public Position(int id, string name, int sal, byte isDel)
+        {
+            positionID = id;
+            positionName = name;
+            salary = sal;
+            isDeleted = isDel;
+
+        }
+        public int PositionID { get { return this.positionID; } set { this.positionID = value; } }
+        public string PositionName { get { return this.positionName; } set { this.positionName = value; } }
+        public int Salary { get { return this.salary; } set { this.salary = value; } }
+        public byte IsDeleted { get { return this.isDeleted; } set { this.isDeleted = value; } }
+
     }
 }
